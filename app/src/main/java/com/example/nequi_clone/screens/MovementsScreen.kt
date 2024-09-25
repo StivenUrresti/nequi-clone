cad7bce1e3759ab109fbbb886d439756f1c62e68
Stiven Urresti <106106455+StivenUrresti@users.noreply.github.com>
2024-09-24 23:20:10 -0500

    create the MovementsScreen

---
+++ app/src/main/java/com/example/nequi_clone/screens/MovementsScreen.kt
@@ -0,0 +1,17 @@
+package com.example.nequi_clone.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MovementsScreen() {
+    Text(text = "hola desde movimientos")
+}
+
+
+@Preview()
+@Composable
+fun MovementsScreenPreview() {
+    MovementsScreen()
+}
---
+++ app/src/main/java/com/example/nequi_clone/screens/PinCodeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.nequi_clone.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable()
+fun PinCodeScreen (modifier: Modifier = Modifier){
+
+
+}
+
+@Preview()
+@Composable
+fun PinCodeScreenPreview(){
+    PinCodeScreen()
+}
